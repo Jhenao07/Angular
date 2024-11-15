6be4802c5f2c02932c731dfdff25b118edbd766a
JorgeHenao <jorgehenao117@gmail.com>
2024-11-14 19:46:32 -0500

    Fin de seccion 4

--- 02-bases/bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/bases/src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name : string = 'Ironman'
+  public age  : number = 45;
+
+  get capitalizedname():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name}  - ${this.age}`;
+  }
+
+  changeHero():void{
+    this.name="Spiderman";
+  }
+
+  changeAge():void{
+    this.age=25;
+  }
+
+  resetform():void{
+  // this.name="iroman";
+    this.age=45;
+
+    document.querySelectorAll ('h1')!.forEach( element => {
+      element.innerHTML = '<h1>Desde Angular</h1>'
+    })
+
+  }
 
 }
